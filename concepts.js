970b5582bf6578d9ce91b281a989ae3fc2fc0ba2
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-18 15:05:53 -0600

    Metodo Math para operaciones matematicas

---
+++ concepts.js
@@ -0,0 +1,47 @@
+
+// Aquí tienes una lista de todos los métodos disponibles en el objeto Math en JavaScript:
+
+// Métodos trigonométricos:
+
+// Math.sin(x): Devuelve el seno de x (en radianes).
+// Math.cos(x): Devuelve el coseno de x (en radianes).
+// Math.tan(x): Devuelve la tangente de x (en radianes).
+// Math.asin(x): Devuelve el arco seno de x (en radianes).
+// Math.acos(x): Devuelve el arco coseno de x (en radianes).
+// Math.atan(x): Devuelve el arco tangente de x (en radianes).
+// Math.atan2(y, x): Devuelve el arco tangente del cociente y / x (en radianes).
+
+// Métodos exponenciales y logarítmicos:
+
+// Math.exp(x): Devuelve e^x, donde e es la base del logaritmo natural.
+// Math.log(x): Devuelve el logaritmo natural de x.
+// Math.log10(x): Devuelve el logaritmo en base 10 de x.
+// Math.log2(x): Devuelve el logaritmo en base 2 de x.
+// Math.pow(base, exponente): Eleva base a la potencia exponente.
+
+// Métodos de redondeo:
+
+// Math.round(x): Redondea x al entero más cercano.
+// Math.floor(x): Redondea x al entero menor o igual más cercano.
+// Math.ceil(x): Redondea x al entero mayor o igual más cercano.
+// Math.trunc(x): Devuelve la parte entera de x (elimina la parte decimal).
+
+// Métodos de valor absoluto:
+
+// Math.abs(x): Devuelve el valor absoluto de x.
+
+// Métodos de generación de números aleatorios:
+
+// Math.random(): Devuelve un número decimal pseudoaleatorio entre 0 (inclusive) y 1 (exclusivo).
+
+// Métodos de constantes matemáticas:
+
+// Math.PI: Devuelve el valor de π (pi).
+// Math.E: Devuelve la constante de Euler (e).
+// Métodos de operaciones matemáticas adicionales:
+
+// Math.min(x1, x2, ..., xn): Devuelve el valor más bajo entre los argumentos proporcionados.
+// Math.max(x1, x2, ..., xn): Devuelve el valor más alto entre los argumentos proporcionados.
+// Math.sqrt(x): Devuelve la raíz cuadrada de x.
+// Math.cbrt(x): Devuelve la raíz cúbica de x.
+// Estos son los métodos disponibles en el objeto Math en JavaScript para realizar diversas operaciones matemáticas. Puedes utilizar estos métodos para llevar a cabo cálculos matemáticos en tus programas.
