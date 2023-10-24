c8313f71e0ae9dbd2b9e6e070e8e7c159d0613a3
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-23 20:36:34 -0600

    Funcion del metodo sort, usando como ejemplo un if

--- practico/platzimath.js
+++ practico/platzimath.js
@@ -34,4 +34,21 @@ function calcularPromedio(lista) {
 
     const promedio = sumaLista / lista.length;
     return promedio;
-};
+}
+
+function ordenarLista(listaDesordenada) {
+    function ordenarListaSort(valorAcumulado, nuevoValor) {
+        if (valorAcumulado > nuevoValor) {
+            return 1;
+        } else if (valorAcumulado == nuevoValor) {
+            return 0;
+        } else if (valorAcumulado < nuevoValor) {
+            return -1;
+        }
+    }
+
+    const lista = listaDesordenada.sort(ordenarListaSort);
+    
+    
+    return lista; 
+}
