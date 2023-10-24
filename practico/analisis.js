36a53f7f113b858f53d6a57250345afe4ba46678
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-24 12:09:20 -0600

    Proyeccion de incremento salarial por persona para el prox year

--- practico/analisis.js
+++ practico/analisis.js
@@ -23,3 +23,24 @@ function medianaPorPersona(nombrePersona) {
     console.log(medianaSalarios);
     return medianaSalarios;
 }
+
+function proyeccionPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+    let porcentajesCrecimiento = [];
+
+    for (let index = 1; index < trabajos.length; index++) {
+        const salarioActual = trabajos[index].salario;
+        const salarioPasado = trabajos[index - 1].salario;
+        const crecimiento = salarioActual - salarioPasado;
+        const porcentajeCrecimiento = crecimiento / salarioPasado;
+        porcentajesCrecimiento.push(porcentajeCrecimiento);
+    }
+
+    const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+    const ultimoSalario = trabajos[trabajos.length - 1].salario;
+    const aumentoSalario = ultimoSalario * medianaPorcentajesCrecimiento;
+    const nuevoSalario = ultimoSalario + aumentoSalario;
+
+    return nuevoSalario;
+}
