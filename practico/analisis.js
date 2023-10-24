aa0c98753da975fc83879536e2b529facf5d476e
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-24 11:39:38 -0600

    Calculando mediana de salario de una persona

--- practico/analisis.js
+++ practico/analisis.js
@@ -1,1 +1,25 @@
 console.log(salarios);
+
+// Analisis personal para Juanita
+
+function encontrarPersona(personaEnBusqueda) {
+    return salarios.find(persona => persona.name == personaEnBusqueda);
+
+    // const persona = salarios.find((persona) => { // Otra manera de realizarlo.
+    //     return persona.name == personaEnBusqueda;
+    // });
+    // return persona;
+}
+
+function medianaPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+    
+    const salarios = trabajos.map(function (elemento) {
+        return elemento.salario;
+    });
+
+    const medianaSalarios = PlatziMath.calcularMediana(salarios);
+
+    console.log(medianaSalarios);
+    return medianaSalarios;
+}
