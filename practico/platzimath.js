7d9c2a2cfa2626ed7924295a6244450f75b33c08
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-23 19:16:14 -0600

    Calculando promedio con funtion y metodo .reduce();

--- practico/platzimath.js
+++ practico/platzimath.js
@@ -1,12 +1,17 @@
 let listaNumber = [1,2,3,4,5,6];
 
 function calcularPromedio(lista) {
-    let sumaLista = 0;
-    for (let index = 0; index < lista.length; index++) {
-        sumaLista = sumaLista + lista[index];
+    // let sumaLista = 0;
+    // for (let index = 0; index < lista.length; index++) {
+    //     sumaLista = sumaLista + lista[index];
+    // }
+
+    function sumarElements(valorAcumulado, nuevoValor) {
+        return valorAcumulado + nuevoValor;
     }
 
+    const sumaLista = lista.reduce(sumarElements);
+
     const promedio = sumaLista / lista.length;
-    console.log(promedio);
     return promedio;
 };
